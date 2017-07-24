9b80c585d4b790421c7e73ae5c77b621bbeaed83
Chris Travers <chris.travers@gmail.com>
2017-07-24 15:20:17 +0200

    Fixing paths for replication

--- t/helpers/travis-setup.sh
+++ t/helpers/travis-setup.sh
@@ -7,8 +7,8 @@ psql -c 'ALTER SYSTEM SET max_replication_slots to 5'
 psql -c 'ALTER SYSTEM SET wal_level to replica'
 sudo pg_createcluster $PGVERSION replica
 sudo service postgresql stop
-sudo rm -rf ~/$PGVERSION/replica 
-sudo sudo -u postgres cp -r ~/$PGVERSION/main ~/$PGVERSION/replica 
-sudo cp t/helpers/recovery.conf ~/$PGVERSION/replica
-sudo chown postgres  ~/$PGVERSION/replica/recovery.conf
+sudo rm -rf ~postgres/$PGVERSION/replica 
+sudo -u postgres cp -r ~postgres/$PGVERSION/main ~/$PGVERSION/replica 
+sudo cp t/helpers/recovery.conf ~postgres/$PGVERSION/replica
+sudo chown postgres  ~postgres/$PGVERSION/replica/recovery.conf
 sudo service postgresql start $PGVERSION
