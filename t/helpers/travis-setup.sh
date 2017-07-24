4210d3cb8d0a62990193e12f00f52bedd5503be5
Chris Travers <chris.travers@gmail.com>
2017-07-24 15:12:52 +0200

    First effort at replica support

--- t/helpers/travis-setup.sh
+++ t/helpers/travis-setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+PGVERSION=9.6
+
 psql -c 'ALTER SYSTEM SET max_wal_senders to 5'
 psql -c 'ALTER SYSTEM SET max_replication_slots to 5'
 psql -c 'ALTER SYSTEM SET wal_level to replica'
+sidp pg_createcluster $PGVERSION replica
 sudo service postgresql stop
-sudo service postgresql start 9.6
+sudo -u postgres rm -rf ~/$PGVERSION/replica 
+sudo -u postgres cp -r ~/$PGVERSION/main ~/$PGVERSION/replica 
+sudo cp t/helpers/recovery.conf ~/$PGVERSION/replica
+sudo chown postgres  ~/$PGVERSION/replica/recovery.conf
+sudo service postgresql start $PGVERSION
