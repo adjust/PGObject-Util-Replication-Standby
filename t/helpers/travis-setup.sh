706f9b73713f094b86846e923671ae402c42cad4
Chris Travers <chris.travers@gmail.com>
2017-07-24 15:50:07 +0200

    further efforts to get recovery going

--- t/helpers/travis-setup.sh
+++ t/helpers/travis-setup.sh
@@ -9,6 +9,7 @@ sudo pg_createcluster $PGVERSION replica
 sudo service postgresql stop
 sudo rm -rf ~postgres/$PGVERSION/replica 
 sudo -u postgres cp -r ~postgres/$PGVERSION/main ~postgres/$PGVERSION/replica 
-sudo cp t/helpers/recovery.conf ~postgres/$PGVERSION/replica
+sudo cp t/helpers/config/recovery.conf ~postgres/$PGVERSION/replica
+sudo cp t/hel[ers/config/replica.conf /etc/postgresql/$PGVERSION/replica/conf.d/replica.conf
 sudo chown postgres  ~postgres/$PGVERSION/replica/recovery.conf
 sudo service postgresql start $PGVERSION
