139002093c304cf41f699b2e9bea922ca3afa5d0
Chris Travers <chris.travers@gmail.com>
2017-07-24 15:17:09 +0200

    Fixing replication permissions

--- t/helpers/travis-setup.sh
+++ t/helpers/travis-setup.sh
@@ -5,10 +5,10 @@ PGVERSION=9.6
 psql -c 'ALTER SYSTEM SET max_wal_senders to 5'
 psql -c 'ALTER SYSTEM SET max_replication_slots to 5'
 psql -c 'ALTER SYSTEM SET wal_level to replica'
-sidp pg_createcluster $PGVERSION replica
+sudo pg_createcluster $PGVERSION replica
 sudo service postgresql stop
-sudo -u postgres rm -rf ~/$PGVERSION/replica 
-sudo -u postgres cp -r ~/$PGVERSION/main ~/$PGVERSION/replica 
+sudo rm -rf ~/$PGVERSION/replica 
+sudo sudo -u postgres cp -r ~/$PGVERSION/main ~/$PGVERSION/replica 
 sudo cp t/helpers/recovery.conf ~/$PGVERSION/replica
 sudo chown postgres  ~/$PGVERSION/replica/recovery.conf
 sudo service postgresql start $PGVERSION
