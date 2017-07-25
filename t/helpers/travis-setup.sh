ffdf6b6e224fa9b695c2e9877e5f62349b60c297
Chris Travers <chris.travers@gmail.com>
2017-07-25 12:42:23 +0200

    Adding a restart to PostgreSQL to try to force a wal flush.

--- t/helpers/travis-setup.sh
+++ t/helpers/travis-setup.sh
@@ -5,13 +5,14 @@ PGVERSION=9.6
 psql -c 'ALTER SYSTEM SET max_wal_senders to 5'
 psql -c 'ALTER SYSTEM SET max_replication_slots to 5'
 psql -c 'ALTER SYSTEM SET wal_level to replica'
+sudo service restart postgresql 9.6;
 sudo pg_createcluster $PGVERSION replica
 sudo service postgresql stop
 sudo rm -rf ~postgres/$PGVERSION/replica 
 sudo -u postgres cp -r ~postgres/$PGVERSION/main ~postgres/$PGVERSION/replica 
 sudo cp t/helpers/config/recovery.conf ~postgres/$PGVERSION/replica
 sudo sh -c "cat t/helpers/config/replica.conf >> /etc/postgresql/$PGVERSION/replica/postgresql.conf"
-sudo sh -c "cat t/helpers/config/main.conf >> /etc/postgresql/$PGVERSION/main/postgresql.conf"
+#sudo sh -c "cat t/helpers/config/main.conf >> /etc/postgresql/$PGVERSION/main/postgresql.conf"
 sudo sh -c "echo 'local replication	postgres	trust' >> /etc/postgresql/$PGVERSION/main/pg_hba.conf"
 sudo service postgresql start $PGVERSION
 echo 'sleeping for 3 sec'
