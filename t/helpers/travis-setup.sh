4a932d50ec1a4df784c7002c76701609e5f9977d
Chris Travers <chris.travers@gmail.com>
2017-07-25 12:38:40 +0200

    Adding config directly to postgresql.conf

--- t/helpers/travis-setup.sh
+++ t/helpers/travis-setup.sh
@@ -11,6 +11,7 @@ sudo rm -rf ~postgres/$PGVERSION/replica
 sudo -u postgres cp -r ~postgres/$PGVERSION/main ~postgres/$PGVERSION/replica 
 sudo cp t/helpers/config/recovery.conf ~postgres/$PGVERSION/replica
 sudo sh -c "cat t/helpers/config/replica.conf >> /etc/postgresql/$PGVERSION/replica/postgresql.conf"
+sudo sh -c "cat t/helpers/config/main.conf >> /etc/postgresql/$PGVERSION/main/postgresql.conf"
 sudo sh -c "echo 'local replication	postgres	trust' >> /etc/postgresql/$PGVERSION/main/pg_hba.conf"
 sudo service postgresql start $PGVERSION
 echo 'sleeping for 3 sec'
