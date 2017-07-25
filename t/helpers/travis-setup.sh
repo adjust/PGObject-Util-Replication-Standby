e33f6c33b4e9cdb7c2248bd5d6a9f0e4c54e0a93
Chris Travers <chris.travers@gmail.com>
2017-07-25 12:31:42 +0200

    fixing shell escapes with sudo

--- t/helpers/travis-setup.sh
+++ t/helpers/travis-setup.sh
@@ -10,8 +10,8 @@ sudo service postgresql stop
 sudo rm -rf ~postgres/$PGVERSION/replica 
 sudo -u postgres cp -r ~postgres/$PGVERSION/main ~postgres/$PGVERSION/replica 
 sudo cp t/helpers/config/recovery.conf ~postgres/$PGVERSION/replica
-sudo sh -c 'cat t/helpers/config/replica.conf >> /etc/postgresql/$PGVERSION/replica/postgresql.conf'
-sudo sh -c 'echo local replication postgres trust >> /etc/postgresql/$PGVERSION/main/pg_hba.conf'
+sudo sh -c "cat t/helpers/config/replica.conf >> /etc/postgresql/$PGVERSION/replica/postgresql.conf"
+sudo sh -c "echo 'local replication	postgres	trust' >> /etc/postgresql/$PGVERSION/main/pg_hba.conf"
 sudo service postgresql start $PGVERSION
 echo 'sleeping for 3 sec'
 sleep 3
