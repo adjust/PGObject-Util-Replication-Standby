c58022293488bacd30eb1917e0fbf66cf6cff6e6
Chris Travers <chris.travers@gmail.com>
2017-07-28 14:23:12 +0200

    Removing some unnecessary lines, testing again on travis

--- t/helpers/travis-setup.sh
+++ t/helpers/travis-setup.sh
@@ -15,8 +15,6 @@ sudo service postgresql stop
 sudo pg_lsclusters 
 sudo pg_createcluster $PGVERSION main2 # PORT 5433
 sudo sh -c "cat t/helpers/config/main.conf >> /etc/postgresql/$PGVERSION/main2/postgresql.conf"
-sudo service postgresql start 9.6
-sudo service postgresql restart 9.6
 sudo pg_ctlcluster $PGVERSION main2 start
 sudo -u postgres createuser -s -p 5433 travis &>/dev/null
 
@@ -29,7 +27,6 @@ cp -r ~postgres/$PGVERSION/main2 ~postgres/$PGVERSION/replica
 sudo cp t/helpers/config/recovery.conf ~postgres/$PGVERSION/replica
 sudo sh -c "cat t/helpers/config/replica.conf >> /etc/postgresql/$PGVERSION/replica/postgresql.conf"
 sudo sh -c "echo 'local replication	postgres	trust' >> /etc/postgresql/$PGVERSION/main2/pg_hba.conf"
-sudo pg_ctlcluster $PGVERSION main2 start
 sudo service postgresql start $PGVERSION
 
 #diagnostics and more
